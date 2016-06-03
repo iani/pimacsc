a8237e63dd3636c6db2708800c95605c00158077
iani <zannos@gmail.com>
2016-06-03 21:31:52 +0300

    +scel, sclang

---
+++ sclang/000TEST.el
@@ -0,0 +1,13 @@
+
+;; "/Users/iani/MEGA/001DEV/EMACS/IZ/sclang/000TEST.el"
+
+;;; Code:
+;; (load-file "/Users/iani/MEGA/001DEV/EMACS/IZ/sclang/000TEST.el")
+;; (message "test")
+;; (message load-file-name)
+;; (message (buffer-file-name (current-buffer)))
+(message "the load path is: %s" load-file-name)
+;; (message (load-path))
+
+
+;;; 000TEST.el ends here
